1e522371c21c0c310bc4248b494c031a79b04d96
Nikhil Verma <varma.nikhil22@gmail.com>
2012-07-20 20:15:26 +0530

    Changes according to webfaction

--- citysom/settings.py
+++ citysom/settings.py
@@ -58,6 +58,13 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'unix:/home/charleshenri/memcached.sock',
+    }
+}
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_URL =  '/media/'
@@ -67,7 +74,7 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+STATIC_ROOT = "/home/charleshenri/webapps/citysom_static/"
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
@@ -78,6 +85,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     (PROJECT_PATH + '/media/'),
+        
 )
 
 # List of finder classes that know how to find static files in
@@ -193,4 +201,4 @@ try:
 except:
     pass
 
-HOST = "citysom.clientpreview.us"
+HOST = "www.citysom.com"
