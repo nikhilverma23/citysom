245a15d6ac678f5af98a9d1d999966e4464bc323
charleshenri <charles.henault@gmail.com>
2012-08-14 15:54:59 -0400

    Modify STATICFILES_DIRS so as not to collect user uploaded images in collectstatic.

--- citysom/settings.py
+++ citysom/settings.py
@@ -84,7 +84,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/charleshenri/webapps/django/citysom/citysom/media',
+    '/home/charleshenri/webapps/django/citysom/citysom/media/css',
+    '/home/charleshenri/webapps/django/citysom/cistysom/media/js',
         
 )
 
