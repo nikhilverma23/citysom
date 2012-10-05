bbcc77477f1778f400474e57e36035b42711ac9c
Nikhil Verma <varma.nikhil22@gmail.com>
2012-10-05 18:49:45 +0530

    Adding all fields of event table from script

--- citysom/scripts/Import_test_events.py
+++ citysom/scripts/Import_test_events.py
@@ -4,6 +4,7 @@ import os
 import sys
 import datetime
 import dateutil
+from datetime import datetime
 from dateutil import rrule
 # Add the current directory to Python's system path.
 cwd = os.path.split(os.getcwd())[0]
@@ -19,7 +20,6 @@ PerformanceDetails, EventGenre, EventPublic, Days
 from django.contrib.auth.models import User
 from django.db import connection
 
-#import pdb;pdb.set_trace();
 #Init environment, variables
 file=sys.path[1] + '/testing.csv'
 i=0
@@ -41,13 +41,133 @@ for row in reader:
 		curr_audience, created =EventPublic.objects.get_or_create(choicelist=row[3])
 		
 		curr_place, created = Place.objects.get_or_create(venue_name=row[6], street=row[7], state=row[8])
+		days = row[34].split()
+		days1 = row[39].split()
+		days2 = row[44].split()
+		days3 = row[49].split()
+		days4 = row[54].split()
+		days_list= []
+		days_list1= []
+		days_list2= []
+		days_list3= []
+		days_list4= []
+		for d in days1:
+		    if "MO" in d:
+		        days_list1.append("1")
+		    elif "FR" in d:
+		    	days_list1.append("5")
+		    elif "SA" in d:
+		    	days_list1.append("6")
+		    elif "SU" in d:
+	        	days_list1.append("7")
+		    elif "TU" in d:
+		        days_list1.append("2")
+		    elif "WE" in d:
+		        days_list1.append("3")
+		    elif "TH" in d:
+	        	days_list1.append("4")
 		
-		#curr_day, created = Days.objects.get_or_create(event_event_by_day=row[34])
-		#from django.db import connection
-		#connection.close()
-		from datetime import datetime
-		d = datetime.strptime(row[20], '%d/%m/%y')
-		event_start_date = d.strftime('%Y-%m-%d')
+		for d in days2:
+		    if "MO" in d:
+		        days_list2.append("1")
+		    elif "FR" in d:
+		    	days_list2.append("5")
+		    elif "SA" in d:
+		    	days_list2.append("6")
+		    elif "SU" in d:
+	        	days_list2.append("7")
+		    elif "TU" in d:
+		        days_list2.append("2")
+		    elif "WE" in d:
+		        days_list2.append("3")
+		    elif "TH" in d:
+	        	days_list2.append("4")
+		
+		for d in days3:
+		    if "MO" in d:
+		        days_list3.append("1")
+		    elif "FR" in d:
+		    	days_list3.append("5")
+		    elif "SA" in d:
+		    	days_list3.append("6")
+		    elif "SU" in d:
+	        	days_list3.append("7")
+		    elif "TU" in d:
+		        days_list3.append("2")
+		    elif "WE" in d:
+		        days_list3.append("3")
+		    elif "TH" in d:
+	        	days_list3.append("4")
+		
+		for d in days4:
+		    if "MO" in d:
+		        days_list4.append("1")
+		    elif "FR" in d:
+		    	days_list4.append("5")
+		    elif "SA" in d:
+		    	days_list4.append("6")
+		    elif "SU" in d:
+	        	days_list4.append("7")
+		    elif "TU" in d:
+		        days_list4.append("2")
+		    elif "WE" in d:
+		        days_list4.append("3")
+		    elif "TH" in d:
+	        	days_list4.append("4")
+	        	
+		for d in days:
+		    if "MO" in d:
+		        days_list.append("1")
+		    elif "FR" in d:
+		    	days_list.append("5")
+		    elif "SA" in d:
+		    	days_list.append("6")
+		    elif "SU" in d:
+	        	days_list.append("7")
+		    elif "TU" in d:
+		        days_list.append("2")
+		    elif "WE" in d:
+		        days_list.append("3")
+		    elif "TH" in d:
+	        	days_list.append("4")
+	    
+		
+		old_event_start_date = datetime.strptime(row[20], '%d/%m/%Y')
+		event_start_date = old_event_start_date.strftime('%Y-%m-%d')
+		
+		old_event_complete_date = datetime.strptime(row[21], "%d/%m/%Y")
+		event_complete_date = old_event_complete_date.strftime('%Y-%m-%d')
+		
+		old_event_start_date1 = datetime.strptime(row[22], '%d/%m/%Y')
+		event_start_date1 = old_event_start_date1.strftime('%Y-%m-%d')
+		
+		old_event_complete_date1 = datetime.strptime(row[23], "%d/%m/%Y")
+		event_complete_date1 = old_event_complete_date.strftime('%Y-%m-%d')
+		
+		old_event_start_date2 = datetime.strptime(row[24], '%d/%m/%Y')
+		event_start_date2 = old_event_start_date2.strftime('%Y-%m-%d')
+		
+		old_event_complete_date2 = datetime.strptime(row[25], "%d/%m/%Y")
+		event_complete_date2 = old_event_complete_date2.strftime('%Y-%m-%d')
+		
+		old_event_start_date3 = datetime.strptime(row[26], '%d/%m/%Y')
+		event_start_date3 = old_event_start_date3.strftime('%Y-%m-%d')
+		
+		old_event_complete_date3 = datetime.strptime(row[27], "%d/%m/%Y")
+		event_complete_date3 = old_event_complete_date3.strftime('%Y-%m-%d')
+		
+		old_event_start_date4 = datetime.strptime(row[28], '%d/%m/%Y')
+		event_start_date4 = old_event_start_date4.strftime('%Y-%m-%d')
+		
+		old_event_complete_date4 = datetime.strptime(row[29], "%d/%m/%Y")
+		event_complete_date4 = old_event_complete_date4.strftime('%Y-%m-%d')
+		
+		old_event_start_date5 = datetime.strptime(row[30], '%d/%m/%Y')
+		event_start_date5 = old_event_start_date5.strftime('%Y-%m-%d')
+		
+		old_event_complete_date5 = datetime.strptime(row[31], "%d/%m/%Y")
+		event_complete_date5 = old_event_complete_date5.strftime('%Y-%m-%d')
+		
 		curr_event, created = Event.objects.get_or_create(title=row[0],\
 														eventwebsite = row[9],
 														description=row[4],\
@@ -67,40 +187,40 @@ for row in reader:
 #														start_hours_on_sunday = row[1],\
 #														end_hours_on_sunday = row[2],\
 														event_start_date = event_start_date,\
-#														event_completion_date=row[21],\
-#														event_start_date1 = row[22],\
-#														event_completion_date1=row[23],\
-#														event_start_date2 = row[24],\
-#														event_completion_date2=row[25],\
-#														event_start_date3 = row[26],\
-#														event_completion_date3=row[27],\
-#														event_start_date4 = row[28],\
-#														event_completion_date4=row[29],\
-#														event_start_date5 = row[30],\
-#														event_completion_date5=row[31],\
+														event_completion_date=event_complete_date,\
+														event_start_date1 = event_start_date1,\
+														event_completion_date1=event_complete_date1,\
+														event_start_date2 = event_start_date2,\
+														event_completion_date2=event_complete_date2,\
+														event_start_date3 = event_start_date3,\
+														event_completion_date3=event_complete_date3,\
+														event_start_date4 = event_start_date4,\
+														event_completion_date4=event_complete_date4,\
+														event_start_date5 = event_start_date5,\
+														event_completion_date5=event_complete_date5,\
 														frequency = row[32],\
 														interval = row[33].strip('.0'),\
-#														#by_day=row[34],
+														
 														by_monthday = row[34],\
 														by_month = row[35],
 														frequency1= row[37],\
 														interval1=row[38].strip('.0'),\
-##														by_day1=row[38],\
+														
 														by_monthday1 = row[40],\
 														by_month1=row[41],
 														frequency2= row[42],\
 														interval2=row[43].strip('.0'),\
-##														by_day2 = row[42],\
+														
 														by_monthday2 = row[-12],\
 														by_month2=row[-11],
 														frequency3= row[-10],\
 														interval3=row[-9].strip('.0'),\
-##														by_day3=row[46],\
+														
 														by_monthday3 = row[-7],\
 														by_month3= row[-6],
 														frequency4= row[-5],\
 														interval4=row[-4].strip('.0'),\
-#														by_day4=row[50],\
+														
 														by_monthday4 = row[-2],\
 														by_month4 = row[-1],
 														defaults={
@@ -112,13 +232,17 @@ for row in reader:
 																},
 														
 														)
-		#curr_event.event_genre=curr_genre
-		#curr_event.event_public=curr_audience
-		
 		curr_event.event_genre.add(curr_genre.id)
 		curr_event.event_public.add(curr_audience.id)
-		curr_event.by_day.add(curr_day.id)
-
-
+		for days in days_list:
+			curr_event.by_day.add(days)
+		for days1 in days_list1:
+			curr_event.by_day1.add(days1)
+		for days2 in days_list:
+			curr_event.by_day2.add(days2)
+		for days3 in days_list3:
+			curr_event.by_day3.add(days3)
+		for days4 in days_list4:
+			curr_event.by_day4.add(days4)
 			
-		
+				
