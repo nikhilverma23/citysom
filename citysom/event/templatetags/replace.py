adddd9ecd7696ee33d181906a4512891e8ee7a49
Nikhil Verma <varma.nikhil22@gmail.com>
2012-11-25 00:17:14 +0530

    forgot to add  the template filter file

--- citysom/event/templatetags/replace.py
+++ citysom/event/templatetags/replace.py
@@ -7,34 +7,6 @@ def cut(value, arg):
     return value.replace(arg, '')
 
 
- 
-#class SetVarNode(template.Node):
-# 
-#    def __init__(self, var_name, var_value):
-#        self.var_name = var_name
-#        self.var_value = var_value
-# 
-#    def render(self, context):
-#        try:
-#            value = template.Variable(self.var_value).resolve(context)
-#        except template.VariableDoesNotExist:
-#            value = ""
-#        context[0][self.var_name] = value
-#        return u""
-# 
-#def set_var(parser, token):
-#    """
-#        {% set <var_name>  = <var_value> %}
-#    """
-#    parts = token.split_contents()
-#    if len(parts) < 4:
-#        raise template.TemplateSyntaxError("'set' tag must be of the form:  {% set <var_name>  = <var_value> %}")
-#    return SetVarNode(parts[1], parts[3])
-# 
-#register.tag('set', set_var)
-
-from django import template
-
 class AssignNode(template.Node):
     def __init__(self, name, value):
         self.name = name
@@ -62,3 +34,10 @@ def do_assign(parser, token):
 
 register = template.Library()
 register.tag('assign', do_assign)
+
+from django import template
+register = template.Library()
+
+@register.filter
+def keyvalue(dict, key):    
+    return dict[key]
